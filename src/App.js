d099c8625965d0296a29fc1bc5a1dcb22e427ffc
mateo <mateo.usuga.v21@gmail.com>
2025-07-07 15:00:44 -0500

    feat: creación de estructura inicial con rutas /, /todos y /registro

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        {/* Navegación simple */}
+        <nav>
+          <Link to="/">Inicio</Link>
+          <Link to="/todos">Mis Todos</Link>
+          <Link to="/add">Agregar Todo</Link>
+        </nav>
+
+        {/* Rutas */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todos" element={<TodoList />} />
+          <Route path="/add" element={<AddTodo />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
